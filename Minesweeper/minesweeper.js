7cfd960a49572076e2776e6f9175fc2b21be3a95
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-09-18 07:50:17 -0700

    ee

--- Minesweeper/minesweeper.js
+++ Minesweeper/minesweeper.js
@@ -346,7 +346,7 @@ class Game{
 							}
 						}
 					}
-					if(((rule == Rules.multiple ? mb : b) == this.pgrid[x][y] && e == 0) || (this.pgrid[x][y] == -3 && b + e == 8)){
+					if(((rule == Rules.multiple ? mb : b) == this.pgrid[x][y] && e == 0) || (this.pgrid[x][y] == -3 && e == 0)){
 						ctx.fillStyle = "#808080";
 					}else{
 						ctx.fillStyle = "#000000";
