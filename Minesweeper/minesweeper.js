efb193ae4f64c4584f4bf56d73243ecb2031f04b
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-09-19 07:03:01 -0700

    m

--- Minesweeper/minesweeper.js
+++ Minesweeper/minesweeper.js
@@ -365,7 +365,7 @@ class Game{
 					(this.pgrid[x][y] == -1 ? "B" : 
 					(this.pgrid[x][y] == -3 ? "F" : 
 					(this.pgrid[x][y] == -4 ? "X" :
-					(checked ? this.pgrid[x][y] - b : 
+					(checked ? this.pgrid[x][y] - mb : 
                     this.pgrid[x][y]
                     ))))).toString(),(box*x)+box/4,(box*(y+1)-box/4)
                 );
