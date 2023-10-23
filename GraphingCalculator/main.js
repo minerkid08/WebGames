5cfbfc7fac5359e85a23d3a11282032f93d5e3f5
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2023-10-23 13:49:51 -0400

    e

--- GraphingCalculator/main.js
+++ GraphingCalculator/main.js
@@ -1,7 +1,9 @@
 import {parse, tokenize} from "./parser.js";
 function update(){
+    alert("run");
     let textBox = document.getElementById("expression");
     let text = textBox.value;
+    alert(text);
     let terms = tokenize(text);
     let out = parse(terms);
     alert(out);
